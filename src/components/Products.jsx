a7a500b26dbb25eed555720c26d079573149c28d
McKinley Blezin <mblezin.usca@gmail.com>
2023-09-21 21:51:36 -0400

    modify product component

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Card } from "@mui/material";
-import SingleProduct from "./SingleProduct";
 import { useNavigate } from "react-router-dom";
+
 export default function Products() {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
